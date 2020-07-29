95294b652454ad33dce6f4175ce24ea02a47d401
Vidhi Nagda <nagda.v@northeastern.edu>
2020-07-29 12:51:12 -0400

    Committing entire code for sample app

---
+++ build/precache-manifest.b55f5d6039bbabc6e5d8ed77182f8d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649395dffd8eed2fd948426c7e3ed423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d9aa1440e316bfd63c",
+    "url": "/static/css/main.e2bfd105.chunk.css"
+  },
+  {
+    "revision": "7d9065a736f782816e2d",
+    "url": "/static/js/2.a6ed427a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6ed427a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d9aa1440e316bfd63c",
+    "url": "/static/js/main.a48035f3.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
